1d3b00db0b247759b85fb82684ab0bd22dd6bfa9
cht39482 <1901880@sit.singaporetech.edu.sg>
2020-09-28 00:34:13 +0800

    Steg part 2 work in progress encode & decode function works

--- gui_cybersecA2.py
+++ gui_cybersecA2.py
@@ -7,25 +7,32 @@ import bpcs
 import os
 
 
-def UploadAction():
+def UploadPayload():
     filename = filedialog.askopenfilename()
-    # e1.insert(filename)
     if len(e1.get()) > 0:
         e1.config(state=NORMAL)
         e1.delete(0, END)
-    e1.insert(10, str(filename))
+    e1.insert(1, str(filename))
+
+
+def UploadCover():
+    filename = filedialog.askopenfilename()
+    if len(e2.get()) > 0:
+        e2.config(state=NORMAL)
+        e2.delete(0, END)
+    e2.insert(1, str(filename))
 
 
 def ExtractPayload(file, option, alpha, message=None):
-    if option == "Decode":
+    if option == 1:
         # output = open("decoded_message.txt", "x")
-        tk.Label(window, text="File is saved to:decoded_message.txt").grid(row=3, column=1)
+        tk.Label(window, text="File is saved to:decoded_message.txt").grid(row=2, column=1)
         bpcs.decode(file, "decoded_message.txt", alpha)
         with open('decoded_message.txt') as file:
             data = str(file.read())
             print(data)
         output.insert(tk.END, data)
-    elif option == "Encode" and message is not None:
+    elif option == 2 and message is not None:
         bpcs.encode(file, message, "encoded_file.png", alpha)
     else:
         print("Invalid Option")
@@ -33,47 +40,40 @@ def ExtractPayload(file, option, alpha, message=None):
 
 def ConfirmAction():
     fileext = ["jpg", "jpeg", "png"]
-    filename = e1.get()
-    print(filename[-1:-4:1])
-    if filename[-1:-4:1] == "txt":
-        lab = tk.Label(window, width=30, text=filename.split("/")[-1])
-        lab.grid(row=4, column=1)
-    elif imghdr.what(filename) in fileext:
+    payloadname = e1.get()
+    covername = e2.get()
+    print(payloadname[-1:])
+    if payloadname[-1:-3:1] == "txt":
+        lab = tk.Label(window, width=30, text=payloadname.split("/")[-1])
+        lab.grid(row=3, column=1)
+    elif imghdr.what(payloadname) in fileext:
         canvas = Canvas(window, width=300, height=300)
-        canvas.grid(row=4, column=1)
-        img = PhotoImage(file=filename)
+        canvas.grid(row=3, column=1)
+        img = PhotoImage(file=payloadname)
         canvas.create_image(10, 10, anchor=NW, image=img)
     else:
         lab = tk.Label(window, width=15, text="Incorrect file format")
-        lab.grid(row=4, column=1)
-    ExtractPayload(filename, option, 0.45)
+        lab.grid(row=3, column=1)
+    ExtractPayload(covername, 2, 0.45, message=payloadname)
+    ExtractPayload("encoded_file.png", 1, 0.45)
     window.mainloop()
-
-
-def change_dropdown(*args):
-    global option
-    option = str(tkdropdown.get())
 
 
 window = tk.Tk()
 window.title("Test 1")
 window.geometry("700x700")
-label = tk.Label(window, text="File name").grid(row=0)
+payloadlabel = tk.Label(window, text="Payload file").grid(row=0, column=0)
+coverlabel = tk.Label(window, text="Cover image").grid(row=0, column=3)
 e1 = tk.Entry(window, width=30)
 e1.grid(row=0, column=1)
-output = tk.Text(window, height=50, width=50, bg="light yellow")
-output.grid(row=6, column=1)
-choices = {"Select an Option", "Encode", "Decode"}
-tkdropdown = StringVar(window)
-tkdropdown.set("Select an Option")
-popupMenu = OptionMenu(window, tkdropdown, *choices)
-label1 = Label(window, text="Command").grid(row=1, column=0)
-label2 = Label(window, text="Output text").grid(row=5, column=0)
+e2 = tk.Entry(window, width=30)
+e2.grid(row=0, column=4)
+button3 = tk.Button(window, text='Upload', command=UploadCover).grid(row=0, column=5)
+output = tk.Text(window, height=50, width=30, bg="light yellow")
+output.grid(row=5, column=1, columnspan=4, sticky=tk.W + tk.E)
+outputlabel = Label(window, text="Output text").grid(row=5, column=0, sticky=NW)
 
-popupMenu.grid(row=1, column=1)
-
-tkdropdown.trace('w', change_dropdown)
-button1 = tk.Button(window, text='Upload', command=UploadAction).grid(row=0, column=2)
-button2 = tk.Button(window, text='Submit', command=ConfirmAction).grid(row=2, column=1)
+button1 = tk.Button(window, text='Upload', command=UploadPayload).grid(row=0, column=2)
+button2 = tk.Button(window, text='Submit', command=ConfirmAction).grid(row=1, column=2)
 
 window.mainloop()
